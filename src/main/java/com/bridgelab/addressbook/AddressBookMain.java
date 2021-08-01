28844d03d5c413e3cfbfbf7179eaad4ffb406322
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-02 02:47:20 +0530

    [mihir] Added: adding new contact to addressbook.

--- src/main/java/com/bridgelab/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelab/addressbook/AddressBookMain.java
@@ -1,9 +1,52 @@
 package com.bridgelab.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain
 {
+    Scanner scanner;
+
+    public AddressBookMain()
+    {
+        scanner = new Scanner(System.in);
+    }
+
+    /**
+     * Name : chooseOption
+     *
+     * Description : Displaying option to user to choose from.
+     *
+     * Algorithm : In this method displaying user multiple option from switch statement.
+     * according to users choice respective method will get called.
+     *
+     * Modification : First commit 14-July-2021
+     */
+    public void chooseOption()
+    {
+        AddressBookService addressBookService = new AddressBookService();
+        boolean isExit = false;
+        while (!isExit)
+        {
+            System.out.print("Enter Number As Per Your Choice:\n1.Add-New Contacts:\n2.Exit:\n");
+            int userInput = scanner.nextInt();
+            switch (userInput)
+            {
+                case 1:
+                    addressBookService.addNewContact();
+                    break;
+                case 2:
+                    //Exit from program.
+                    isExit = true;
+                    break;
+                default:
+                    System.out.println("Please enter valid input");
+            }
+        }
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Welcome To Address Book !");
+        AddressBookMain addressBookMain = new AddressBookMain();
+        addressBookMain.chooseOption();
     }
 }
--- src/main/java/com/bridgelab/addressbook/AddressBookService.java
+++ src/main/java/com/bridgelab/addressbook/AddressBookService.java
@@ -1,6 +1,60 @@
 package com.bridgelab.addressbook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookService
 {
-    
+    Scanner scanner;
+    //To store contacts.
+    ArrayList<PersonDetails> contactList;
+
+    //instantiating scanner and ArrayList in constructor.
+    public AddressBookService()
+    {
+        scanner = new Scanner(System.in);
+        contactList = new ArrayList<>();
+    }
+
+    /**
+     * Name : addNewContact
+     *
+     * Description : Adding new contact to AddressBook.
+     *
+     * Algorithm : In this method accepting all necessary contact details from user,
+     * and adding those all details to store in AddressBook as a new contact.
+     *
+     * Modification : First commit 14-July-2021.
+     */
+    public void addNewContact()
+    {
+        PersonDetails contactDetails = new PersonDetails();
+
+        System.out.print("Enter First Name : ");
+        contactDetails.setFirstName(scanner.next());
+
+        System.out.print("Enter Last Name : ");
+        contactDetails.setLastName(scanner.next());
+
+        System.out.print("Enter Address : ");
+        contactDetails.setAddress(scanner.next());
+
+        System.out.print("Enter City : ");
+        contactDetails.setCity(scanner.next());
+
+        System.out.print("Enter State : ");
+        contactDetails.setState(scanner.next());
+
+        System.out.print("Enter ZipCode : ");
+        contactDetails.setZipCode(scanner.nextInt());
+
+        System.out.print("Enter Phone-Number : ");
+        contactDetails.setPhoneNumber(scanner.nextLong());
+
+        System.out.print("Enter Email-Id : ");
+        contactDetails.setEmailId(scanner.next());
+
+        contactList.add(contactDetails);
+        System.out.println("\nContact added successfully.\n");
+    }
 }
